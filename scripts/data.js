18e55a24143cd1610c756e69bd40aa983090ae12
Daraexus <juanpabloreyesg@gmail.com>
2020-07-17 11:42:10 -0500

    added gitignore

--- scripts/data.js
+++
@@ -1,7 +0,0 @@
-import { Curso } from './curso.js';
-import { Aprendiz, NivelEducativo } from './aprendiz.js';
-var cursos = [new Curso("Prácticas esenciales para el agilismo", 20, 90, true, 2019),
-    new Curso("Ingeniería de software para la web", 15, 99, true, 2018),
-    new Curso("Pruebas automatizadas", 25, 50, false, 2020),
-    new Curso("Principios de diseño y arquitectura", 30, 75, true, 2020)];
-export var ap = new Aprendiz("Juan Pablo", "Reyes Gómez", "avatar.png", 30, "Colombia", NivelEducativo.POSGRADO, cursos);
